846091e74fd2295f6572ef4cc1f2250760cc2214
Vittorio Zampinetti <zampinetti@gmail.com>
2020-01-10 12:09:10 +0100

    fixed not feasible starting solution bug

--- src/com/dmogroup5/utils/Solution.java
+++ src/com/dmogroup5/utils/Solution.java
@@ -86,10 +86,10 @@ public class Solution {
 
                 examAssigned = weightedSolution.placeExam(i, randTimetable);
 
-//                if (!examAssigned) {
+                if (!examAssigned) {
 //                    System.out.println("ERROR: exam not assigned to any timeslot");
-//                    break;
-//                }
+                    break;
+                }
             }
         }
 
